1d0b00c42a4643911f125dd1470aa1b706ceff38
Baumwolle25 <Baumwolle25@gmail.com>
2020-10-30 15:09:43 +0100

    now i start setting up the site

--- js/app.js
+++ js/app.js
@@ -12,3 +12,7 @@ let today = new Date();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+
+function btnfunction(inputString){
+  window.alert(inputString);
+}
